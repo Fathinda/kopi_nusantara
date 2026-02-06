7a6b989f01ffb9864d251de9652c890638dda96a
Sanlinex <supergamingtop@gmail.com>
2026-02-06 09:31:18 +0700

    addNavbar

---
+++ resources/views/main/home.blade.php
@@ -0,0 +1,9 @@
+<x-layout>
+    <div class="wrap1">
+        <div class="container">
+           
+            <img src="/assets/ec3f6d04e2c532990f4fdb5ab578cdf8-removebg-preview.png" alt="">
+            <img src="/assets/86327765f9a6cecd4266cf0049f8a720-removebg-preview.png" alt="">
+        </div>
+    </div>
+</x-layout>
