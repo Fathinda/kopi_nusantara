a97fcea6ebaa26c722fa87a9c6bf2430ed48a8f0
Fathin <abyan.cnplus@gmail.com>
2026-02-06 16:31:13 +0700

    aa

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MainController;
 use App\Http\Controllers\AboutController;
-<<<<<<< HEAD
+
 use App\Http\Controllers\AuthController;
 =======
 use App\Http\Controllers\ProductController;
